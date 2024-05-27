014a0212e9076f1bb20130f250edde1ab04f095c
ShivamKrBU <shivamkrbu005@gmail.com>
2024-05-28 00:24:09 +0530

    Laravel

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+       @include('admin.css')
+  </head>
+  <body>
+
+      <!-- partial -->
+
+      @include('admin.sidebar')
+      
+      @include('admin.navbar')
+
+      @include('admin.body')
+      <!-- partial -->
+      
+      @include('admin.script')
+      
+  </body>
+</html>
